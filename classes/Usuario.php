1b3d0095ead41f42a8d63f00f3b95bc07c15241c
Eduardo Bona <eduardobona@vivaweb.net>
2018-08-27 14:15:42 -0300

    enviando projeto para github

---
+++ classes/Usuario.php
@@ -0,0 +1,18 @@
+<?php
+
+// se tiver cumprido o desafio do mapa,
+// uma boa dica de estudo
+// seria criar uma classe para 
+// Usuario
+// com métodos para cadastrar,
+// editar e excluir
+//
+//
+// isso poderá lhe ajudar muito
+// nos estudos e na compreensão da
+// orientação a objetos
+//
+//
+// dúvidas: ead@eduardobona.com.br
+
+?>
